fda7a098b6813424b2507800c3379d60ff26db93
Rajveer188 <veerkajle188@gmail.com>
2025-02-24 13:07:43 +0530

    [Rajveer] Add. create a address book spring project to cater to REST Request from address book UI

---
+++ src/test/java/com/capgemini_training/addressbookapp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini_training.addressbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
