8fa4157bef8d534c748c5eaf36960ff278049ef7
Rajveer188 <veerkajle188@gmail.com>
2025-03-01 12:58:42 +0530

    [Rajveer] Add. Add Validation to Name Field so the REST call can be validated

--- src/main/java/com/capgemini_training/addressbookapp/dto/AddressDTO.java
+++ src/main/java/com/capgemini_training/addressbookapp/dto/AddressDTO.java
@@ -1,5 +1,8 @@
 package com.capgemini_training.addressbookapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,12 @@ import org.springframework.stereotype.Component;
 public class AddressDTO {
     //Attributes
     private int id;
+    @NotBlank(message = "name should not be blank")
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}( [A-Z][a-zA-Z]{2,})?$")
     private String name;
+    @NotBlank(message = "not should not be blank")
+    @Email(message = "Invalid email format")
     private String email;
+    @NotBlank(message = "number should not be blank")
     private long number;
 }
